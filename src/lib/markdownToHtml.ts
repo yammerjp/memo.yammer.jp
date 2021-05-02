91f1532f59d8e0e1c9b5994a48de49acc50a6f81
Keisuke Nakayama <baskk4.dt@gmail.com>
2021-05-02 23:11:06 +0900

    Support table with markdown syntax

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,8 +1,9 @@
 import remark from 'remark';
 import html from 'remark-html';
 import footnotes from 'remark-footnotes';
+import gfm from 'remark-gfm'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(footnotes).use(html).process(markdown);
+  const result = await remark().use(gfm).use(footnotes).use(html).process(markdown);
   return result.toString();
 }
