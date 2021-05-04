03ea3247e3db898a705f4b450f10f537c4cdbc03
Keisuke Nakayama <baskk4.dt@gmail.com>
2021-05-04 21:52:43 +0900

    Fix style

--- src/components/articleCard.tsx
+++ src/components/articleCard.tsx
@@ -2,17 +2,20 @@ import Link from 'next/link'
 import { PostType } from '../types/post'
 import {iso8601toDisplayStr} from '../lib/date'
 import Tags from './tags'
-const ArticleCard = ({ post, tagsEmphasizing }: { post: PostType, tagsEmphasizing: string[] } ) => {
+
+const ArticleCard = ({ post, tagsEmphasizing }: { post: PostType, tagsEmphasizing: string[] }) => {
     return (
         <section className="article-card">
-            <Link href={"/posts/"+ post.slug}>
+            <Link href={"/posts/" + post.slug}>
                 <div className="article-link">
                     <div className="article-date">{iso8601toDisplayStr(post?.date)}</div>
                     <div className="article-title">{post.title}</div>
-                    <div className="article-description">{post.description || ''}</div>
+                    <div className="article-card-left">
+                        <div className="article-description">{post.description || ''}</div>
+                    </div>
                 </div>
             </Link>
-            <Tags tags={post.tags ?? []} tagsEmphasizing={tagsEmphasizing}/>
+            <Tags tags={post.tags ?? []} tagsEmphasizing={tagsEmphasizing} />
         </section>
     )
 }
