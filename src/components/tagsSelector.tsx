0682e575060db1d155510487459db3dcd578a73f
Keisuke Nakayama <baskk4.dt@gmail.com>
2021-05-08 19:41:49 +0900

    Update: 絞り込み用タグ一覧を大叔父小文字関係なくソートする

--- src/components/tagsSelector.tsx
+++ src/components/tagsSelector.tsx
@@ -1,8 +1,17 @@
+// sorting function
+const mixedUpperLower = (a: string, b: string): number => {
+  if (a.toUpperCase() > b.toUpperCase()) {
+    return 1;
+  }
+  return -1;
+}
+
+
 const TagsSelector = ({tagsAll, tagsSelected, clickedTag}: {tagsAll: string[], tagsSelected: string[], clickedTag: (tagName: string, willbeSelected: boolean)=>void}) => {
     return (
         <div className="article-tags-selector">
         {
-            Array.from(new Set(tagsAll)).sort().map((tag:string) => {
+            Array.from(new Set(tagsAll)).sort(mixedUpperLower).map((tag:string) => {
                 const selected = tagsSelected.includes(tag);
                 return (
                     <button key={tag}
