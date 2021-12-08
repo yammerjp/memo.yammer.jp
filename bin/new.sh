827d74d007051ec586e35766d673eec64c548e7a
Keisuke Nakayama <me@yammer.jp>
2021-12-08 23:02:29 +0900

    Update: 新しい記事を作成するスクリプトの引数に、記事のidを受け取れるようにする

--- bin/new.sh
+++ bin/new.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+PROCESS_ARG_1="$1"
+
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 cd "$SCRIPT_DIR"
 cd ..
 
 TODAY_ISO8601="$(gdate --iso-8601=second)"
 TODAY="$(echo "$TODAY_ISO8601"| awk -F T '{ print $1}' | sed 's/-//g')"
-NEW_ARTICLE_PATH="content/posts/$TODAY.md"
+
+if [ "$PROCESS_ARG_1" == "" ]; then
+  NEW_ARTICLE_TITLE="$TODAY"
+else
+  NEW_ARTICLE_TITLE="$PROCESS_ARG_1"
+fi
+
+NEW_ARTICLE_PATH="content/posts/$NEW_ARTICLE_TITLE.md"
 
 if [ -e "$NEW_ARTICLE_PATH" ]; then
   echo "$NEW_ARTICLE_PATH is already exists..." 1>&2
@@ -15,7 +24,7 @@ fi
 
 cat <<-EOF > "$NEW_ARTICLE_PATH"
 ---
-title: "$TODAY"
+title: "$NEW_ARTICLE_TITLE"
 date: "$TODAY_ISO8601"
 tags: [ "日記" ]
 ---
