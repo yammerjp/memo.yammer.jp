e08c010d98d0db37e035c03a65912c351a5102e6
Keisuke Nakayama <me@yammer.jp>
2022-01-26 22:50:18 +0900

    WIP

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -4,18 +4,18 @@ import promises from 'fs/promises'
 async function main() {
   await promises.mkdir('jsonfeed/posts', {recursive: true})
   const feed = await getJsonFeed()
-  await promises.writeFile('jsonfeed/all.json', JSON.stringify(feed))
+  await promises.writeFile('public/jsonfeed/all.json', JSON.stringify(feed))
 
   const feedWithoutContents = await getJsonFeedWithoutContents()
   const slugs = await getPostSlugs()
   for (const slug of slugs) {
     const item = await getJsonFeedItemBySlug(slug)
     const articleUniqueKey = slug.slice(0, -3)
-    await promises.writeFile(`jsonfeed/posts/${articleUniqueKey}.json`, JSON.stringify(item))
-    await promises.writeFile(`jsonfeed/posts/${articleUniqueKey}.raw.html`, item?.content_html || '')
-    await promises.writeFile(`jsonfeed/posts/${articleUniqueKey}.raw.txt`, item?.content_text || '')
+    await promises.writeFile(`public/jsonfeed/posts/${articleUniqueKey}.json`, JSON.stringify(item))
+    await promises.writeFile(`public/jsonfeed/posts/${articleUniqueKey}.raw.html`, item?.content_html || '')
+    await promises.writeFile(`public/jsonfeed/posts/${articleUniqueKey}.raw.txt`, item?.content_text || '')
   }
-  await promises.writeFile(`jsonfeed/summary.json`, JSON.stringify(feedWithoutContents))
+  await promises.writeFile(`public/jsonfeed/summary.json`, JSON.stringify(feedWithoutContents))
 }
 
 main()
