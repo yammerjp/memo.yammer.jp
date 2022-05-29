5704eefe87e068079ef39666883deb72af1b833e
Keisuke Nakayama <me@yammer.jp>
2022-05-29 14:35:15 +0000

    Fix: getPostSlugs() はファイル外で使っていないのでexportしない

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -10,7 +10,7 @@ import { OgImageUrlInText } from './cloudinaryOgp'
 
 import { PostHistoryType } from '../types/post'
 
-export async function getPostSlugs(): Promise<string[]> {
+async function getPostSlugs(): Promise<string[]> {
   const files = await fs.readdir(join(process.cwd(), 'content', 'posts'))
   return files.filter((f) => /.*\.md/.test(f))
 }
