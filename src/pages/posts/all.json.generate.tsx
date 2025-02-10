d8fa3bbec398c41910251d5f5b42f637497b2b13
Keisuke Nakayama <me@yammer.jp>
2025-02-11 01:20:05 +0900

    add /posts/all.json

--- src/pages/posts/all.json.generate.tsx
+++ src/pages/posts/all.json.generate.tsx
@@ -5,8 +5,8 @@ import { getAllPosts } from '../../lib/api'
 const json = () => {
   return (
     <Head>
-      <meta httpEquiv='refresh' content='0; url=/posts/index.json' />
-      <title>redirect to /posts/index.json</title>
+      <meta httpEquiv='refresh' content='0; url=/posts/all.json' />
+      <title>redirect to /posts/all.json</title>
     </Head>
   )
 }
@@ -14,15 +14,15 @@ const json = () => {
 export default json
 
 export const getStaticProps = async () => {
-  const allPosts = await getAllPosts(['title', 'date', 'slug', 'tags', 'description', 'tags'])
+  const allPosts = await getAllPosts(['title', 'date', 'slug', 'tags', 'description', 'tags', 'html', 'history'])
 
 
   const bufObj = {
     version: "https://jsonfeed-extension.yammer.jp/v0.1.0",
     version_description: "a subset of json feed 1.1. it is optional that items.content_text or items.content_html",
-    title: "Posts on memo.yammer.jp",
+    title: "Posts on memo.yammer.jp (full text)",
     home_page_url: "https://memo.yammer.jp",
-    feed_url: "https://memo.yammer.jp/posts/index.json",
+    feed_url: "https://memo.yammer.jp/posts/all.json",
     description: "Recent content in Posts on memo.yammer.jp",
     generator: "memo.yammer.jp with Next.js (https://github.com/yammerjp/memo.yammer.jp)",
     language: "ja",
@@ -33,12 +33,14 @@ export const getStaticProps = async () => {
       url: 'https://memo.yammer.jp/posts/' + item.slug,
       title: item.title,
       summary: item.description ?? '',
+      content_html: item.html,
+      _history: item.history,
       date_published: item.date,
       tags: item.tags,
     })
     )
   }
-  await writeFile(`${process.cwd()}/public/posts/index.json`, JSON.stringify(bufObj))
+  await writeFile(`${process.cwd()}/public/posts/all.json`, JSON.stringify(bufObj))
   return {
     props: {},
   }
